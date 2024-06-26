2d14e698175660bc5d6eadd26f411f74741ff455
Manish I <106602542+AlterHoodie@users.noreply.github.com>
2024-06-26 14:07:09 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setup(
     name="Crawl4AI",
     version="0.2.71",
     description="🔥🕷️ Crawl4AI: Open-source LLM Friendly Web Crawler & Scrapper",
-    long_description=open("README.md").read(),
+    long_description=open("README.md").read(encoding="utf-8"),
     long_description_content_type="text/markdown",
     url="https://github.com/unclecode/crawl4ai",
     author="Unclecode",
